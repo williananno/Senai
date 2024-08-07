7f5af42120ae20db545ef2e633aaac959e666d34
Giovana <giovanaa.carrascosa194@gmail.com>
2024-08-06 21:56:46 -0300

    Primeiro programa

---
+++ primeiro.py
@@ -0,0 +1,11 @@
+# pessoa = input("Digite o nome da pessoa:")
+# print("Boa noite" ,pessoa)
+# idade = input("Qual a sua idade? ")
+# print("Nossa! Você já tem ", idade , "anos")
+# email = input("Digite o seu e-mail: ")
+# print("Obrigado",pessoa, "por ter passado o seu e-mail",email)
+
+
+numero = input ("Digite um número: ")
+outroNumero = input("Digite outro número: ")
+print("A soma é ", float(numero) + float(outroNumero))
